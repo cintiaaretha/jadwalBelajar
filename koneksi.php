4357f846272b67eef13af7a1eb82ef6702efa44e
azkanida <124250030@student.upnyk.ac.id>
2026-05-11 22:52:22 -0300

    menambahkan koneksi

--- koneksi.php
+++ koneksi.php
@@ -1,11 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    hallo
-</body>
-</html>
+<?php
+$host = "localhost";
+$username = "root";
+$password = "";
+$file = "jadwalbelajar";
+
+$koneksi = mysqli_connect($host, $username, $password, $file);
+?>
